06dc2e8bcb7ecaeffffeb94b63cc358cf0589ccf
Julia Winchester <julia.m.winchester@gmail.com>
2016-10-24 22:24:38 -0400

    Index.html and app.js for control page started

---
+++ control/html/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('app', ['ngMaterial']);
+
+angular.module('app').controller('AppController', AppController);
+
+AppController.$inject = ['$scope'];
+
+function AppController($scope) {
+	
+}
+

8be94fd30665509c2aca78309178c0d5c58231dc
Julia Winchester <julia.m.winchester@gmail.com>
2016-10-24 22:37:06 -0400

    Skeleton for test http posts added

--- control/html/app.js
+++ control/html/app.js
@@ -2,9 +2,37 @@ var app = angular.module('app', ['ngMaterial']);
 
 angular.module('app').controller('AppController', AppController);
 
-AppController.$inject = ['$scope'];
+AppController.$inject = ['$scope', '$http'];
 
-function AppController($scope) {
-	
+function AppController($scope, $http) {
+	$scope.wake = function () {
+		return $http.post('/wake', {key: 1337}).then(
+			function successCallback(response) {
+				return;	
+			},
+			function errorCallback(response) {
+				return;
+			});
+	}
+
+	$scope.sleep = function () {
+		return $http.post('/sleep', {key: 1337}).then(
+			function successCallback(response) {
+				return;	
+			},
+			function errorCallback(response) {
+				return;
+			});
+	}
+
+	$scope.reboot = function () {
+		return $http.post('/reboot', {key: 1337}).then(
+			function successCallback(response) {
+				return;	
+			},
+			function errorCallback(response) {
+				return;
+			});
+	}
 }
 

3e991727cb8cc817662c2c8b63172c062d95a3dd
Julia Winchester <Julia.Winchester@stonybrook.edu>
2016-12-10 12:10:38 -0500

    Removed incognito status on status init.sh script

--- mode/status/init.sh
+++ mode/status/init.sh
@@ -2,5 +2,5 @@ export DISPLAY=:0.0
 
 tmux kill-session
 
-tmux new -d -s status 'chromium-browser --noerrdialogs --kiosk ./html/status.html --incognito'
+tmux new -d -s status 'chromium-browser --noerrdialogs --kiosk ./html/status.html'
 

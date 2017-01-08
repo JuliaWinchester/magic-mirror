9c28bc3fe67424921550eacc5491d38ef6af1966
Julie Winchester <julia.m.winchester@gmail.com>
2017-01-08 15:09:58 -0500

    Bug fixes to motion detection tests

--- mode/icu/icu.py
+++ mode/icu/icu.py
@@ -1,16 +1,18 @@
 from picamera.array import PiRGBArray
-from picamera import picamera
+from picamera import PiCamera
 import cv2
 import time
 
 # initialize camera and grab reference to raw capture
 camera = PiCamera(resolution = (640,480), framerate = 16)
+camera.hflip = True
+camera.vflip = True
 raw_capture = PiRGBArray(camera, size = (640, 480))
 time.sleep(2.5)
 
 # capture frames from camera, initializing background model if needed
 avg = None
-for f in camera.capture_continuous(rawCapture, format = 'bgr', use_video_port=True):
+for f in camera.capture_continuous(raw_capture, format = 'bgr', use_video_port=True):
 	frame = f.array
 	motion_detected = 0
 
@@ -19,20 +21,21 @@ for f in camera.capture_continuous(rawCapture, format = 'bgr', use_video_port=Tr
 
 	if avg is None:
 		avg = gray.copy().astype("float")
-		rawCapture.truncate(0)
+		raw_capture.truncate(0)
 		continue
 
 	# accumulate weighted average between current frame and previous frames,
 	# then compute difference between current frame and running average
 	cv2.accumulateWeighted(gray, avg, 0.5)
 	frame_delta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-	thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)
-
-	cv2.imshow('Thresh', thresh)
+	thresh = cv2.threshold(frame_delta, 5, 255, cv2.THRESH_BINARY)
+	print cv2.countNonZero(thresh[1])
+	cv2.imshow('thresh', thresh[1])
 	key = cv2.waitKey(1) & 0xFF
 
 	if key == ord('q'):
 		break
+	raw_capture.truncate(0)
 
 camera.release()
 cv2.destroyAllWindows()
